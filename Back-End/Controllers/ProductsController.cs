f79e61f2aae7e2dc6dfb5360a337daa709201b48
horqueda <93798101+horqueda@users.noreply.github.com>
2022-09-29 00:28:32 -0300

    Primera API
    29/09/2022 Creacion de API

---
+++ Back-End/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Back_End.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Back_End.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet("GetProductsHome")]
+        public ActionResult GetProductsHome()
+        {
+            List<Product> products = new List<Product>();
+            Product product1 = new Product(1, "Producto 1");
+            product1.Description = "Camiseta Seleccion Argentina";
+            product1.Price = 20000;
+            products.Add(product1);
+            
+            Product product2 = new Product(2, "Producto 2");
+            product2.Description = " Camiseta Seleccion Brasil";
+            products.Add(product2);
+
+            Product product3 = new Product(3, "Producto 3");
+            product3.Size = "XL";
+            products.Add(product3); 
+            return Ok(products);
+        }
+    }
+}
---
+++ Back-End/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Back_End.Models
+{
+    public class Product {           
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string UrlImage { get; set; }
+        public double Price { get; set; }
+        public string Size { get; set; }
+
+        public Product(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            this.Size = "No Size";
+        }
+    }
+}
