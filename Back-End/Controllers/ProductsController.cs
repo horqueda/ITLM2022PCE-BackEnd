95181bb826fd49ee535019710c51a332c9a73891
horqueda <93798101+horqueda@users.noreply.github.com>
2022-10-20 11:52:06 -0300

    ProductsControles

--- Back-End/Controllers/ProductsController.cs
+++ Back-End/Controllers/ProductsController.cs
@@ -84,29 +84,34 @@ namespace Back_End.Controllers
         {
             List<Product> products = new List<Product>();
 
-            Product product1 = new Product(1, "Producto 1", 3000, false);
+            Product product1 = new Product(1, "Producto 1", 1000, false);
             product1.Description = "Remera basica blanca";
             product1.category = new Category(1, "Remeras");
             products.Add(product1);
 
-            Product product2 = new Product(2, "Producto 2", 500, true);
+            Product product2 = new Product(2, "Producto 2", 2000, true);
             product2.Description = "Buzo canguro con capucha";
             product2.category = new Category(2, "Buzos");
             products.Add(product2);
 
-            Product product3 = new Product(3, "Producto 3", 1000, false);
+            Product product3 = new Product(3, "Producto 3", 3000, false);
             product3.Size = "XXXL";
             products.Add(product3);
 
-            Product product4 = new Product(2, "Producto 4", 1000, true);
+            Product product4 = new Product(4, "Producto 4", 4000, true);
             product4.Description = "Buzo canguro con capucha 4 ";
-            product4.category = new Category(2, "Buzos");
+            product4.category = new Category(4, "Buzos");
             products.Add(product4);
 
-            Product product5 = new Product(1, "Producto 5", 1000, false);
+            Product product5 = new Product(5, "Producto 5", 5000, false);
             product5.Description = "Remera basica blanca 5";
-            product5.category = new Category(1, "Remeras");
+            product5.category = new Category(5, "Remeras");
             products.Add(product5);
+
+            product product6 = new Product(6, "Producto 6", 6000, false);
+            product6.Description = "Remera basica blanca 5";
+            product6.category = new Category(6, "Remeras");
+            products.Add(product6);
 
             return products;
         }
