a02d9ba6da24322e4c1d09d06233d8efbf81b86f
horqueda <93798101+horqueda@users.noreply.github.com>
2022-11-01 01:07:42 -0300

    PostProductRepository

--- Back-End/Repositories/CartRepository.cs
+++ Back-End/Repositories/CartRepository.cs
@@ -30,29 +30,3 @@ namespace Back_End.Repositories
 
     }
 }
-
-
-
-//using Back_End.Models;
-
-//namespace Back_End.Repositories
-//{
-//    public class CartRepository
-//    {
-//        private static Cart _cart  { get; set; }  // static es para que pueda persistir en memoria. 
-//        public static void AddProductToCart(int idProduct, int cantidad) { 
-//            if (_cart==null)_cart = new Cart();
-//            ProductRepository repo = new ProductRepository();
-//            List<Product> products = new List<Product>();
-//            products = repo.GetProducts();
-//            Product product = products.Find(Prod => Prod.Id == idProduct);
-//            if(product != null)
-//            {
-//                _cart.Items.Add(new CartItem(product, cantidad));
-//            }
-//            throw new Exception("Producto no encontrado"); 
-//        }
-
-//    }
-
-//}
