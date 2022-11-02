044532a36d9867e1ecb9ab4cc93373bc297d8306
horqueda <93798101+horqueda@users.noreply.github.com>
2022-11-02 10:12:51 -0300

    ControladoOk

--- Back-End/Models/Category.cs
+++ Back-End/Models/Category.cs
@@ -16,24 +16,3 @@
         }
     }
 }
-
-
-
-//namespace Back_End.Models
-//{
-//    public class Category
-//    {
-//        public int idCategory { get; set; }
-//        public string name { get; set; }
-
-//        public Category()
-//        {
-
-//        }
-//        public Category(int idCategory, string name)
-//        {
-//            this.idCategory = idCategory;
-//            this.name = name;
-//        }
-//    }
-//}
